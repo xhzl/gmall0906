219afbe7d9e3e83b0f20f13a0d4b688c3857bd50
xhzl <xhzl1175912587@163.com>
2019-01-11 21:32:14 +0800

    common-util拆分

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/UserService.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.service;
+
+
+import com.atguigu.gmall.bean.UserInfo;
+
+import java.util.List;
+
+public interface UserService {
+
+
+    List<UserInfo> selectUserList();
+}
